e5959e2c546867851696c100d0bf0e1ed3ad0a44
SirLectric43 <domingomiguel4343@gmail.com>
2025-11-04 13:46:25 +0100

    cambiosEj2

--- src/app/ColorSwitch.js
+++ src/app/ColorSwitch.js
@@ -1,9 +1,12 @@
-export default function ColorSwitch({
-    onChangeColor
-  }) {
-    return (
-      <button>
-        Cambiar color
-      </button>
-    );
+export default function ColorSwitch({ onChangeColor }) {
+  function handleClick(event) {
+    event.stopPropagation(); 
+    onChangeColor();         
   }
+
+  return (
+    <button onClick={handleClick}>
+      Cambiar color
+    </button>
+  );
+}
