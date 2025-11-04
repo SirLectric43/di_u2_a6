6e359d0b2536d86830cc7f86431f6644118ebc4a
SirLectric43 <domingomiguel4343@gmail.com>
2025-11-04 13:43:57 +0100

    cambiosEj1

--- src/app/LightSwitch.js
+++ src/app/LightSwitch.js
@@ -1,17 +1,16 @@
 export default function LightSwitch() {
-    function handleClick() {
-      let bodyStyle = document.body.style;
-      if (bodyStyle.backgroundColor === 'black') {
-        bodyStyle.backgroundColor = 'white';
-      } else {
-        bodyStyle.backgroundColor = 'black';
-      }
+  function handleClick() {
+    let bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor === 'black') {
+      bodyStyle.backgroundColor = 'white';
+    } else {
+      bodyStyle.backgroundColor = 'black';
     }
-  
-    return (
-      <button onClick={handleClick()}>
-        Alterna las luces
-      </button>
-    );
   }
-  
+
+  return (
+    <button onClick={handleClick}>
+      Alterna las luces
+    </button>
+  );
+}
